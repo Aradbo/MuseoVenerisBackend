9216d6c805790123f5ce228b3c0fd3f216ed1ac9
Aradbo <arlethadyani1202@gmail.com>
2025-11-29 23:15:04 -0600

    cambio nombre controlador de metodoPago

--- src/routes/metodoPago.routes.js
+++ src/routes/metodoPago.routes.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const router = express.Router();
 
-const metodoPagoController = require("../controllers/metodoPago.controller");
+const metodoPagoController = require("../controllers/metodoPago.Controller");
 
 // GET ALL
 router.get("/", metodoPagoController.getAll);
